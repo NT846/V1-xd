6e812c1bc128624d4d3847216237b798625b07ee
NT846 <96948477+NT846@users.noreply.github.com>
2022-04-17 11:49:30 +0700

    Add files via upload

---
+++ assets/preload/data/intervention/script.lua
@@ -0,0 +1,11 @@
+function onCreate()
+
+addLuaScript('lua_characters/gfstand')
+setProperty('gfstand.x', 700)
+setProperty('gfstand.y', 0)
+
+addLuaScript('lua_characters/bfbside')
+setProperty('bfbside.x', 1450)
+setProperty('bfbside.y', 650)
+
+end
